7a5db2b4beccc20fb8a364dd70756dd9c9aa0091
MFactor1 <mingsqu@gmail.com>
2025-02-28 18:07:05 -0500

    Fully functional push-based server

--- server.ts
+++ server.ts
@@ -14,11 +14,14 @@ const wss = new WebSocketServer({ server });
 
 const validRequests: Set<string> = new Set();
 validRequests.add("increment");
+validRequests.add("updateRequest");
 
 let seedsMatt: number[] = new Array(9).fill(0);
 let seedsHail: number[] = new Array(9).fill(0);
 let updateMatt: boolean[] = new Array(seedsMatt.length).fill(false);
 let updateHail: boolean[] = new Array(seedsHail.length).fill(false);
+let lastDataUpdate: number = Date.now();
+let lastClientUpdate: number = Date.now();
 
 redis.lrange("seedsMatt", 0, -1, (err, value) => {
   if (value && value.length == seedsMatt.length) {
@@ -37,6 +40,17 @@ redis.lrange("seedsHail", 0, -1, (err, value) => {
 });
 
 async function syncToRedis() {
+  const existsMatt = await redis.exists("seedsMatt");
+  const existsHail = await redis.exists("seedsHail");
+
+  if (!existsMatt) {
+    await redis.rpush("seedsMatt", ...seedsMatt);
+  }
+
+  if (!existsHail) {
+    await redis.rpush("seedsHail", ...seedsHail);
+  }
+
   for (var i = 0; i < seedsMatt.length; i++) {
     if (updateMatt[i]) {
       await redis.lset("seedsMatt", i, seedsMatt[i]);
@@ -51,8 +65,24 @@ async function syncToRedis() {
 
 setInterval(syncToRedis, 5000);
 
+function updateClients() {
+  if (lastClientUpdate < lastDataUpdate) {
+    console.log("Sending out updates to clients");
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: "seedUpdate", matt: seedsMatt, hail: seedsHail }));
+      }
+    });
+    lastClientUpdate = Date.now();
+  }
+}
+
+setInterval(updateClients, 1000);
+
 wss.on("connection", (ws) => {
-  ws.send(JSON.stringify({ type: "counter", matt: seedsMatt, hail: seedsHail }));
+  console.log("Client connected");
+
+  ws.send(JSON.stringify({ type: "seedUpdate", matt: seedsMatt, hail: seedsHail }));
 
   ws.on("message", async (message) => {
     const data = JSON.parse(message.toString());
@@ -60,6 +90,8 @@ wss.on("connection", (ws) => {
       ws.send(JSON.stringify({ type: "error", message: "Missing type field", code: 400 }));
       return;
     }
+
+    console.log(`Recieved message of type: ${data.type}`);
 
     if (!validRequests.has(data.type)) {
       ws.send(JSON.stringify({ type: "error", message: `Unrecognized request type: ${data.type}`, code: 400 }));
@@ -73,22 +105,27 @@ wss.on("connection", (ws) => {
       }
 
       if (!Array.isArray(data.matt) || !Array.isArray(data.hail)) {
-        ws.send(JSON.stringify({ type: "error", message: "data.matt and data.hail must be an arrays", code: 400 }));
+        ws.send(JSON.stringify({ type: "error", message: "data.matt and data.hail must be an arrays", code: 422 }));
       }
 
       if (data.matt.length != seedsMatt.length || data.hail.length != seedsHail.length) {
-        ws.send(JSON.stringify({ type: "error", message: "Invalid data length", code: 400 }));
+        ws.send(JSON.stringify({ type: "error", message: "Invalid data length", code: 422 }));
         return;
       }
 
       for (var i = 0; i < data.matt.length; i++) {
         if (typeof data.matt[i] !== "number") {
-          ws.send(JSON.stringify({ type: "error", message: `Invalid data format in data.matt: ${typeof data.matt[i]}`, code: 400 }));
+          ws.send(JSON.stringify({ type: "error", message: `Invalid data format in data.matt: ${typeof data.matt[i]}`, code: 422 }));
+          return;
         }
         if (typeof data.hail[i] !== "number") {
-          ws.send(JSON.stringify({ type: "error", message: `Invalid data format in data.hail: ${typeof data.hail[i]}`, code: 400 }));
+          ws.send(JSON.stringify({ type: "error", message: `Invalid data format in data.hail: ${typeof data.hail[i]}`, code: 422 }));
+          return;
         }
       }
+
+      console.log(data.matt);
+      console.log(data.hail);
 
       for (var i = 0; i < seedsMatt.length; i++) {
         if (data.matt[i] != 0) {
@@ -101,7 +138,18 @@ wss.on("connection", (ws) => {
         }
       }
 
+      lastDataUpdate = Date.now();
+
+    } else if (data.type == "updateRequest") {
       ws.send(JSON.stringify({ type: "seedUpdate", matt: seedsMatt, hail: seedsHail }));
     }
   });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server listening on http://localhost:3001");
 });
